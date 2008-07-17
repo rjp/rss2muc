e7d4b267c76ac971563a6ab169c6fa3bf9cd4a64
U-PAPALOCTOPUS\rjp <rjp@papaloctopus.(none)>
2008-07-17 15:52:58 +0100

    - get our list of feed URLs from a database

--- rss2muc.rb
+++ rss2muc.rb
@@ -1,8 +1,18 @@
 require 'rubygems'
 require 'feed-normalizer'
 require 'open-uri'
+require 'sqlite3'
 
-feed = FeedNormalizer::FeedNormalizer.parse open('frontpage.xml')
-feed.entries.each { |f|
-    puts f.title
+feeds = []
+
+db = SQLite3::Database.new('feeds.db')
+db.execute('select url from feed') { |f|
+    feeds.push f[0]
+}
+
+feeds.each { |feed_url|
+    feed = FeedNormalizer::FeedNormalizer.parse open(feed_url)
+	feed.entries.each { |f|
+	    puts f.title
+	}
 }
