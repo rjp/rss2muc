a9ef8e37e558510b60c613d61da3d79607c195d5
U-PAPALOCTOPUS\rjp <rjp@papaloctopus.(none)>
2008-07-20 11:15:32 +0100

    - vaguely attempt to keep track of what we already saw

--- rss2muc.rb
+++ rss2muc.rb
@@ -2,17 +2,26 @@ require 'rubygems'
 require 'feed-normalizer'
 require 'open-uri'
 require 'sqlite3'
+require 'sha1'
 
 feeds = []
 
 db = SQLite3::Database.new('feeds.db')
-db.execute('select url from feeds') { |f|
-    feeds.push f[0]
+
+db.execute('select url, last from feeds') { |f|
+    feeds.push [ f[0], f[1] ]
 }
 
-feeds.each { |feed_url|
+feeds.each { |feed_url, feed_last|
+    new_entries = []
     feed = FeedNormalizer::FeedNormalizer.parse open(feed_url)
 	feed.entries.each { |f|
-	    puts f.title
+        sha1 = SHA1.hexdigest(f.title + f.url)
+        if feed_last == sha1 then
+            new_entries = []
+        else
+            new_entries.push f.title + " " + sha1
+        end
 	}
+    p new_entries
 }
