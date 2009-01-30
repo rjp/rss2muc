5c965999085fcbceb8073a76629b0858bd7be6ee
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-30 18:20:43 +0000

    - perpetual loop, checking every 60 seconds

--- rss2muc.rb
+++ rss2muc.rb
@@ -7,15 +7,19 @@ require 'sha1'
 feeds = GDBM.new('feeds.db')
 articles = GDBM.new('read.db')
 
-feeds.keys.each { |feed_url|
-    new_entries = []
-    feed = FeedNormalizer::FeedNormalizer.parse open(feed_url)
-	feed.entries.each { |f|
-        # do we care if the title changes?
-        sha1 = SHA1.hexdigest(f.title + f.url)
-        unless articles[sha1] then
-            new_entries.push f.title + " " + sha1
-        end
+loop {
+	feeds.keys.each { |feed_url|
+	    new_entries = []
+	    feed = FeedNormalizer::FeedNormalizer.parse open(feed_url)
+		feed.entries.each { |f|
+	        # do we care if the title changes?
+	        sha1 = SHA1.hexdigest(f.title + f.url)
+	        unless articles[sha1] then
+	            new_entries.push f.title + " " + sha1
+	            articles[sha1] = f.title
+	        end
+		}
+	    p new_entries
 	}
-    p new_entries
+    sleep 60
 }
