ad6f28b95876911e833904ca1ee5eba77d9cf9cd
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-30 08:32:58 +0000

    - sqlite3 to gdbm refactoring

--- rss2muc.rb
+++ rss2muc.rb
@@ -7,16 +7,11 @@ require 'sha1'
 feeds = GDBM.new('feeds.db')
 articles = GDBM.new('read.db')
 
-db = SQLite3::Database.new('feeds.db')
-
-db.execute('select url, last from feeds') { |f|
-    feeds.push [ f[0], f[1] ]
-}
-
-feeds.each { |feed_url, feed_last|
+feeds.keys.each { |feed_url|
     new_entries = []
     feed = FeedNormalizer::FeedNormalizer.parse open(feed_url)
 	feed.entries.each { |f|
+        # do we care if the title changes?
         sha1 = SHA1.hexdigest(f.title + f.url)
         unless articles[sha1] then
             new_entries.push f.title + " " + sha1
